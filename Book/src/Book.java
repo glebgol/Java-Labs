4fbc568284ac3c407001dd75fcf727562534ccd2
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-09-29 09:22:34 +0300

    Added book comparator and compareTo method

--- Book/src/Book.java
+++ Book/src/Book.java
@@ -2,8 +2,16 @@ public class Book implements Comparable<Book>{
     public String Name;
     public String Author;
 
+    public Book(String name, String author) {
+        Name = name;
+        Author = author;
+    }
     @Override
     public int compareTo(Book book) {
         return Name.compareTo(book.Name);
+    }
+    @Override
+    public String toString() {
+        return Name + " " + Author;
     }
 }
--- Book/src/Main.java
+++ Book/src/Main.java
@@ -1,5 +1,30 @@
+import java.lang.reflect.Array;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book("Java", "Horstmann"));
+        books.add(new Book("Разочарованный цветок", "Bob Mirskiy"));
+        books.add(new Book("Евгений Онегин", "Пушкин"));
+        books.add(new Book("Мертвые души", "Гоголь"));
+
+        books.sort(new BookByAuthorComparator());
+        System.out.println("Книги, отсортированные по автору:");
+        for (var book : books) {
+            System.out.println(book);
+        }
+
+        System.out.println();
+
+        Collections.sort(books);
+        System.out.println("Книги, отсортированные по названию:");
+        for (var book : books) {
+            System.out.println(book);
+        }
     }
 }
