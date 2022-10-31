ef7a975b8da1cdee62b724cb8028936ee4798fb6
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-10-31 16:00:41 +0300

    Added unit tests

--- SumOfRow/src/Main.java
+++ SumOfRow/src/Main.java
@@ -2,28 +2,6 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-    public static double GetSumOfRow(double x, int k) {
-        if (x >= 1 || x <= -1) {
-            throw new IllegalArgumentException("Argument x should be in (-1, 1)!");
-        }
-        else if (k <= 0) {
-            throw new IllegalArgumentException("Argument k should be natural number!");
-        }
-        double e = Math.pow(10, -(k + 1));
-        double sum = 1;
-        double element = 0.5 * x;
-        int r = 1;
-        while (true) {
-            sum += element;
-            element = -1 * element * x * (double) r / (r + 3);
-            r += 2;
-            if (Math.abs(element) < e) {
-                break;
-            }
-        }
-        return sum;
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = "";
@@ -41,7 +19,7 @@ public class Main {
                 str = in.next();
                 int k = Integer.parseInt(str);
 
-                System.out.println(formatter.format(GetSumOfRow(num, k)));
+                System.out.println(formatter.format(SumOfRow.GetSumOfRow(num, k)));
                 System.out.println(formatter.format(Math.sqrt(1 + num)));
             } catch (NumberFormatException ex) {
                 System.out.println(str + " is not a number!");
---
+++ SumOfRow/src/SumOfRowTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SumOfRowTest {
+
+    @ParameterizedTest
+    //@ValueSource(doubles = )
+    void SumOfRow_IllegalArgument_ThrowsIllegalArgumentException() {
+    }
+}
