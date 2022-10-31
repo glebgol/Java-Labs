4ea02bc012732da7e3c1147899af8d1de16ecf56
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-10-31 18:15:40 +0300

    Counted the sum of string numbers

--- HelloJavaFX/src/main/java/com/example/hellojavafx/HelloController.java
+++ HelloJavaFX/src/main/java/com/example/hellojavafx/HelloController.java
@@ -2,13 +2,18 @@ package com.example.hellojavafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField inputLine;
+    @FXML
+    protected void onCalculateButtonClick() {
+        var inputText = inputLine.getCharacters().toString();
+        var result = SumOfString.GetSumOfNumbersRegEx(inputText);
+        welcomeText.setText("Sum of string numbers: " + result);
     }
 }
---
+++ HelloJavaFX/src/main/java/com/example/hellojavafx/SumOfString.java
@@ -0,0 +1,30 @@
+package com.example.hellojavafx;
+
+import java.util.StringTokenizer;
+
+public class SumOfString {
+    public static double GetSumOfNumbers(String line) {
+        StringTokenizer st = new StringTokenizer(line, " \t\n\r,");
+        double sum = 0;
+        while(st.hasMoreTokens()){
+            double num = 0;
+            try {
+                num = Double.parseDouble(st.nextToken());
+                sum += num;
+            } catch (NumberFormatException ignored) {}
+        }
+        return sum;
+    }
+    public static double GetSumOfNumbersRegEx(String line) {
+        StringTokenizer st = new StringTokenizer(line, " \t\n\r,");
+        double sum = 0;
+        while(st.hasMoreTokens()){
+            String el = st.nextToken();
+            if (el.matches("(^-|^\\+)?[0-9]+((\\.)?[0-9]*(e[0-9]+)?)?$")) {
+                double num = Double.parseDouble(el);
+                sum += num;
+            }
+        }
+        return sum;
+    }
+}
