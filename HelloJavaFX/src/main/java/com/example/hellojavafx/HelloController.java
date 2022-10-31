da48cda8a3852cde95abcad8f54729abe0827d8a
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-10-31 19:08:29 +0300

    Calculated string numbers, added Alert for result

--- HelloJavaFX/src/main/java/com/example/hellojavafx/HelloController.java
+++ HelloJavaFX/src/main/java/com/example/hellojavafx/HelloController.java
@@ -1,8 +1,16 @@
 package com.example.hellojavafx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 
 public class HelloController {
@@ -11,9 +19,21 @@ public class HelloController {
     @FXML
     private TextField inputLine;
     @FXML
+    private Label resultLabel;
+    @FXML
     protected void onCalculateButtonClick() {
         var inputText = inputLine.getCharacters().toString();
         var result = SumOfString.GetSumOfNumbersRegEx(inputText);
-        welcomeText.setText("Sum of string numbers: " + result);
+        ShowResultMessage(result);
+    }
+
+    private void ShowResultMessage(double result) {
+        var contentResult = "Sum of string number: " + result;
+        var info = new Alert(Alert.AlertType.INFORMATION);
+        info.initStyle(StageStyle.DECORATED);
+        info.setTitle("Result");
+        info.setHeaderText("Successful");
+        info.setContentText(contentResult);
+        info.showAndWait();
     }
 }
--- HelloJavaFX/src/main/java/com/example/hellojavafx/ResultController.java
+++
@@ -1,4 +0,0 @@
-package com.example.hellojavafx;
-
-public class ResultController {
-}
