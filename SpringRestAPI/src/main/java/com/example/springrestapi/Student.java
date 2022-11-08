130923ef052daab4001173730a559e157d41f94b
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-11-08 19:11:00 +0300

    Created StudentRepository

--- SpringRestAPI/src/main/java/com/example/springrestapi/Student.java
+++ SpringRestAPI/src/main/java/com/example/springrestapi/Student.java
@@ -1,22 +1,40 @@
 package com.example.springrestapi;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
-    private long id;
+
+    private @Id @GeneratedValue Long id;
     private String name;
-
-    public Student(long id, String name) {
+    private int age;
+    public void setId(Long id) {
         this.id = id;
+    }
+    public Student(String name, int age) {
         this.name = name;
+        this.age = age;
     }
-
-    public Student() {
-    }
-
-    public long getId() {
+    @Id
+    public Long getId() {
         return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
